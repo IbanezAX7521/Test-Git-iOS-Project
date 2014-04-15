c39e335246590fe55d40654ac728d51a5efa6f98
Jeffrey Linn <Jeffers@Jeffreys-MacBook-Pro.local>
2014-04-15 23:02:24 +0200

    added a test class

---
+++ JALTest.h
@@ -0,0 +1,13 @@
+//
+//  JALTest.h
+//  Git Test
+//
+//  Created by Jeffrey Linn on 4/15/14.
+//  Copyright (c) 2014 His Lordship. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JALTest : NSObject
+
+@end
---
+++ JALTest.m
@@ -0,0 +1,13 @@
+//
+//  JALTest.m
+//  Git Test
+//
+//  Created by Jeffrey Linn on 4/15/14.
+//  Copyright (c) 2014 His Lordship. All rights reserved.
+//
+
+#import "JALTest.h"
+
+@implementation JALTest
+
+@end
